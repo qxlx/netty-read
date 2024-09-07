81db6ce5268a81f7458af3b7a84d41b8f7b0ae37
qxlx <qxlxi@qq.com>
2024-09-07 21:39:56 +0800

    threadlocal 源码学习

---
+++ netty-socre-read/example/src/main/java/io/netty/qxlxi/ChannelFutureDemo.java
@@ -0,0 +1,15 @@
+package io.netty.qxlxi;
+
+import io.netty.channel.ChannelFuture;
+
+/**
+ * @author qxlx
+ * @date 2024/9/7 14:55
+ */
+public class ChannelFutureDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- netty-socre-read/example/src/main/java/io/netty/qxlxi/TestByteBuffer.java
+++ netty-socre-read/example/src/main/java/io/netty/qxlxi/TestByteBuffer.java
@@ -1,6 +1,8 @@
 package io.netty.qxlxi;
 
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
@@ -36,11 +38,13 @@ public class TestByteBuffer {
     public static void main(String[] args) throws InterruptedException {
 //        ByteBuffer byteBuffer = ByteBuffer.allocate(10 * 1024 * 1024);
 //
-        System.out.println(unsafe);
-        long address = unsafe.allocateMemory(100 * 1024 * 1024);
-        System.out.println(address);
-
-        TimeUnit.MINUTES.sleep(1);
+//        System.out.println(unsafe);
+//        long address = unsafe.allocateMemory(100 * 1024 * 1024);
+//        System.out.println(address);
+//
+//        TimeUnit.MINUTES.sleep(1);
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(10);
+        System.out.println(buffer);
     }
 
 }
